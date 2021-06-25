69556c8800eaf1761baeefde4c8b0f23d2362134
ahmetkca <ahmet.karapinar@ontariotechu.net>
2021-06-25 15:11:38 -0400

    conditionally set vite configs based on mode dev or prod

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,33 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // const fs = require('fs')
 import * as fs from 'fs';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  server: {
-    open: true,
-    https: {
-      key: fs.readFileSync('./localhost-key.pem'),
-      cert: fs.readFileSync('./localhost.pem')
+export default ({ mode }) => {
+  if (mode === "development") {
+    return {
+      plugins: [svelte()],
+      server: {
+        open: true,
+        https: {
+          key: fs.readFileSync('./localhost-key.pem'),
+          cert: fs.readFileSync('./localhost.pem')
+        }
+      }
+    }
+  } else if (mode === "production") {
+    return {
+      plugins: [svelte()]
     }
   }
-})
+}
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [svelte()],
+//   server: {
+//     open: true,
+//     https: {
+//       key: fs.readFileSync('./localhost-key.pem'),
+//       cert: fs.readFileSync('./localhost.pem')
+//     }
+//   }
+// })
