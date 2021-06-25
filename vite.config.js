7a62099a507ee3e3966985e9c46174593d67a547
ahmetkca <ahmet.karapinar@ontariotechu.net>
2021-06-25 14:48:28 -0400

    instead of using CommonJs to import fs, used es module with import

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-const fs = require('fs')
+// const fs = require('fs')
+import * as fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
